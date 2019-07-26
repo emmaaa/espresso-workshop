e4213bfb547319f9705e1750dfb0629a388fe40f
Emma Burton <emma.burton@bbc.co.uk>
2019-07-26 16:36:35 +0100

    Add another espresso exercise

--- app/src/androidTest/java/com/example/espressoworkshop/c/LoginTest.kt
+++ app/src/androidTest/java/com/example/espressoworkshop/c/LoginTest.kt
@@ -2,20 +2,32 @@ package com.example.espressoworkshop.c
 
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.hasErrorText
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import com.example.espressoworkshop.R
 import com.example.espressoworkshop.b.TestCase
+import com.example.espressoworkshop.c.gherkin.`when`.When
 import com.example.espressoworkshop.c.gherkin.given.Given
 import org.junit.Test
 
 class LoginTest : TestCase() {
 
     @Test
-    fun launchAppTest() {
+    fun badLoginTest() {
         Given.user.launchesTheApp()
 
-        onView(withId(R.id.password_field)).check(matches(isDisplayed()))
+        When.user.entersIncorrectLogin()
+
+        //todo this is failing and i don't know why...
+        onView(withId(R.id.email_field)).check(matches(hasErrorText("Incorrect email")))
+        onView(withId(R.id.email_field)).check(matches(hasErrorText("Incorrect password")))
+
+    }
+
+    @Test
+    fun successfulLoginTest() {
+
+
     }
 
 }
--- app/src/androidTest/java/com/example/espressoworkshop/c/gherkin/when/UserActions.kt
+++ app/src/androidTest/java/com/example/espressoworkshop/c/gherkin/when/UserActions.kt
@@ -1,3 +1,24 @@
 package com.example.espressoworkshop.c.gherkin.`when`
 
-object UserActions
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import com.example.espressoworkshop.R
+
+object UserActions {
+
+    fun entersCorrectLogin() {
+        onView(withId(R.id.email_field)).perform(typeText("letmein@please.com"))
+        onView(withId(R.id.password_field)).perform(typeText("super_secure"))
+
+        onView(withId(R.id.sign_in_button)).perform(click())
+    }
+
+    fun entersIncorrectLogin() {
+        onView(withId(R.id.email_field)).perform(typeText("errrm"))
+        onView(withId(R.id.password_field)).perform(typeText("no idea"))
+
+        onView(withId(R.id.sign_in_button)).perform(click())
+    }
+}
